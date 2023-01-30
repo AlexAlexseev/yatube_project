da283069c1c971617aaaa6b56edee854bbe567ce
Алексеев Александр <alexxseev@yandex.ru>
2023-01-30 17:46:46 +0500

    add posts

--- Yatube/posts/views.py
+++ Yatube/posts/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Главная страница
 def index(request):    
     return HttpResponse('Ты <i>не можешь</i> получить правильные <b>ответы</b>,<br> '
-        'если у тебя нет правильных <s>вопросов</s> запросов.')
+                        'если у тебя нет правильных <s>вопросов</s> запросов.')
 
 def group_posts(request, slug):
     return HttpResponse(f'Тут будет пост с названием: {slug}')
