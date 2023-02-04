fea26856ac6031e439f1416d821f44de9a786c7e
Алексеев Александр <alexxseev@yandex.ru>
2023-02-04 16:25:40 +0500

    add model Group

--- Yatube/posts/models.py
+++ Yatube/posts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 # Из модуля auth импортируем функцию get_user_model 
 from django.contrib.auth import get_user_model
 
+#Group = get_user_model()
 User = get_user_model() 
 
 class Post(models.Model):
@@ -12,3 +13,21 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     ) 
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.CASCADE,
+        #Не обязательно указывать сообщество 'Group'
+        blank=True,
+        null=True,
+        related_name='group'
+    )
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    description = models.TextField()
+    def __str__(self) -> str:
+        return self.title
+    
+
